d716d3315e1161a961befeae60909b93b75a9825
forsythrl <tarrantrl@utexas.edu>
2020-10-05 19:35:44 -0500

    update getRole for manager class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 // require Manager class
 const Manager = require('../lib/Manager');
 
@@ -11,4 +11,12 @@ test('creates a manager object',() => {
     expect(manager.id).toBe('jn1');
     expect(manager.email).toBe('jan@work.com');
     expect(manager.officeNumber).toBe('PAR 3');
+})
+
+// test the getRole function
+test('checks the role of the manager', () => {
+    // create a manager object instance
+    const manager = new Manager('Jan', 'jn1', 'jan@work.com', 'PAR 3');
+    // check that getRole returns manager
+    expect(manager.getRole()).toBe('Manager');
 })
